998e409a319db5fc27148ac633f32168a36ffbfc
Sweta Tosti <swetat007@protonmail.com>
2022-12-05 06:56:02 +0100

    My favourite songs list using array Ext school ex Module 8 Ex 7

---
+++ arrays.py
@@ -0,0 +1,46 @@
+
+#These are my favorite songs.
+
+song_fav = ["tu hi re !",
+            "lailä main laila",
+            "tere binna ",
+            "nazar ke samane"]
+
+#Sort it by alphabetical order.
+
+song_fav.sort()
+
+#Display each song in it on its own line in the terminal. 
+
+print(song_fav[0])
+print(song_fav[1])
+print(song_fav[2])
+print(song_fav[3])
+
+
+#Add a 5th song at the end of the array.
+
+song_fav.append("kabhi to nazar")
+
+#Display how many songs are in the array inside a sentence such as 
+# "There are x songs in the array."
+
+print("There are " + str(len(song_fav)) + " songs in the array.")
+
+#Remove the 3rd song in the array.
+
+song_fav.pop(2)
+
+#Display the content of the array on one line.
+
+print(song_fav)
+
+#Display the length of the array.
+
+print(len(song_fav))
+
+
+
+
+
+
